76cdbfb3708ba3829f6f5ec78ffefed92073d5ad
Mostafa-Aljazar <mostafaibrahim20032020@gmail.com>
2025-08-26 10:24:47 +0300

    get params in client side using use hook in article page

--- src/app/(pages)/(landing)/blog/[article_id]/page.tsx
+++ src/app/(pages)/(landing)/blog/[article_id]/page.tsx
@@ -1,18 +1,20 @@
+'use client';
 import { TYPE_WRITTEN_CONTENT } from '@/@types/actors/common-types/index.type';
 import Article_Story from '@/components/landing/common/article-story/article-story';
 import { Stack } from '@mantine/core';
+import { use } from 'react';
 
 export default async function Article_Page({
   params,
 }: {
   params: Promise<{ article_id: string }>;
 }) {
-  const { article_id } = await params;
+  const { article_id } = use(params);
 
   return (
     <Stack pt={60} className='w-full' mih={'100vh'}>
       <Article_Story
-        written_content_Id={parseInt(article_id)}
+        written_content_Id={Number(article_id)}
         destination={TYPE_WRITTEN_CONTENT.BLOG}
       />
     </Stack>
