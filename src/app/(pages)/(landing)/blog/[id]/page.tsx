0d76ea765eaec437ad6c666ff3810eb1c381a831
Mostafa-Aljazar <mostafaibrahim20032020@gmail.com>
2025-08-26 10:57:29 +0300

    rename article_Id to id

--- src/app/(pages)/(landing)/blog/[id]/page.tsx
+++ src/app/(pages)/(landing)/blog/[id]/page.tsx
@@ -4,15 +4,12 @@ import Article_Story from '@/components/landing/common/article-story/article-sto
 import { Stack } from '@mantine/core';
 import { use } from 'react';
 
-export default function Article_Page({ params }: { params: Promise<{ article_id: string }> }) {
-  const { article_id } = use(params);
+export default function Article_Page({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = use(params);
 
   return (
     <Stack pt={60} className='w-full' mih={'100vh'}>
-      <Article_Story
-        written_content_Id={Number(article_id)}
-        destination={TYPE_WRITTEN_CONTENT.BLOG}
-      />
+      <Article_Story written_content_Id={parseInt(id)} destination={TYPE_WRITTEN_CONTENT.BLOG} />
     </Stack>
   );
 }
